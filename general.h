3586bb32ed05d80b34fc6735d41735396b89da3b
Leonardo Falabella <leo.falabella01@gmail.com>
2024-02-19 13:25:05 +0100

    First commit

---
+++ general.h
@@ -0,0 +1,42 @@
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double randoum_double(){
+    // Returns a random real in [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max){
+    // Returns a random real in [min,max)
+    return min + (max-min)*randoum_double();
+}
+
+// Common Headers
+
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "general.h"
+
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+
+int main() {
+    
+    //World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    //Camera
+    camera cam;
+
+    cam.aspect_ratio = 16.0/9.0;
+    cam.image_width = 400;
+
+    //Render
+    cam.render(world);
+
+}
+
