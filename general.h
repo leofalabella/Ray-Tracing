0495e1a42f8c550285d9f61f830462e107c67a47
Leonardo Falabella <leo.falabella01@gmail.com>
2024-02-26 20:38:27 +0100

    Combined commit

--- general.h
+++ general.h
@@ -2,9 +2,17 @@
 #define GENERAL_H
 
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
 #include <limits>
 #include <memory>
-#include <cstdlib>
+
+
+// Common Headers
+
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
 
 // Usings
 
@@ -23,20 +31,14 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-inline double randoum_double(){
+inline double random_double(){
     // Returns a random real in [0,1)
     return rand() / (RAND_MAX + 1.0);
 }
 
 inline double random_double(double min, double max){
     // Returns a random real in [min,max)
-    return min + (max-min)*randoum_double();
+    return min + (max-min)*random_double();
 }
-
-// Common Headers
-
-#include "interval.h"
-#include "ray.h"
-#include "vec3.h"
 
 #endif
