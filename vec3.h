d208a11438c1b04777b05fb2357c4985db399705
Mateus Galvão <56044273+Mateushga1@users.noreply.github.com>
2024-03-21 14:10:35 +0100

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -152,5 +152,4 @@ inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat){
     vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
     return r_out_perp + r_out_parallel;
 }
-
 #endif
